c7da4c0096dcfe18bd25d18ae50f2c1544efe533
Righteous1ice <hoplabradley@gmail.com>
2023-06-05 20:50:27 +0100

    displaying player symbols now

--- script.js
+++ script.js
@@ -1,84 +1,76 @@
 ////////// CREATE PLAYER \\\\\\\\\\
 
-const createPlayer = (name, marker) => {
-    return { name, marker };
+const createPlayers = (name1, name2) => {
+    const player1 = {
+      name: name1,
+      symbol: "X"
+    };
+  
+    const player2 = {
+      name: name2,
+      symbol: "O"
+    };
+  
+    return { player1, player2 };
   };
+  
+  const players = createPlayers("Player 1", "Player 2");
 
+
+  const currentPlayerSymbol = (() => {
+    let currentPlayer = players.player1;
+  
+    return () => {
+      if (currentPlayer === players.player1) {
+        currentPlayer = players.player2;
+      } else {
+        currentPlayer = players.player1;
+      }
+      return currentPlayer.symbol;
+    };
+})();
 
 
 ////////// GAMEBOARD OBJECT \\\\\\\\\\
 const gameBoard = (() => {
-    let board = [];
-    for (let i = 0; i < 9; i++) {
-      board.push(" ");
-    }
-  
-    let gameBoardContainer = document.querySelector('.gameboard');
-  
-    board.forEach((item, index) => {
-      const tile = document.createElement('div');
-      tile.className = 'tile';
-      gameBoardContainer.appendChild(tile);
-    });
-
-
-    let tile = gameBoardContainer.querySelectorAll('.tile');
-
-    tiles.forEach((item, index) => {
-        tile.addEventListener('click', () => {
-            if(tile.tileContent === ' ') {
-                const player = currentPlayerSymbol();
-                tile.tileContent  = player;
-            }
-        })
-
-    })
-  
-    return board;
-  })();
-  
-  console.log(gameBoard);
-  
-
-
-
-
-
-
-
-
-/*
-
-
-////////// CREATE PLAYER \\\\\\\\\\
-
-const createPlayer = (name, maker) => {
-  return { name, maker };
-};
-
-
-
-////////// GAMEBOARD OBJECT \\\\\\\\\\ 
-const gameBoard = (() => {
   let board = [];
   for (let i = 0; i < 9; i++) {
-    board.push(" ");
+      board.push(" ");
   }
 
-  let tiles = document.querySelector('.tiles');
+  let gameBoardContainer = document.getElementById('gameboard');
 
   board.forEach((item, index) => {
-    const tile = document.createElement('div');
-    tile.className = 'tiles';
-
-    tiles.appendChild(tile);
+      const tile = document.createElement('div');
+      tile.className = 'tile';
+      tile.dataset.tileContent = "";
+      gameBoardContainer.appendChild(tile);
   });
 
-  return board;
+  let tiles = gameBoardContainer.querySelectorAll('.tile');
+
+  tiles.forEach((tile, index) => {
+      tile.addEventListener('click', () => {
+          if (tile.dataset.tileContent === "") {
+              const player = currentPlayerSymbol();
+              tile.dataset.tileContent = player;
+              tile.textContent = player;
+          }
+      });
+  });
+
+  // Return the game board object
+  return {
+      board: board,
+      tiles: tiles
+  };
 })();
 
 console.log(gameBoard);
 
 
+  ////////// DISPLAY \\\\\\\\\\
 
-*/
+  const infoDisplay = document.getElementById('info');
+  
+  
