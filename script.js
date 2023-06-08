e07dd2b4621f14d330b2e48a2ef99ce9bce79825
Righteous1ice <hoplabradley@gmail.com>
2023-06-08 18:13:27 +0100

    Added logic to check for a winner based of winning combo

--- script.js
+++ script.js
@@ -42,6 +42,44 @@ const gameBoard = (() => {
 
   let gameBoardContainer = document.getElementById('gameboard');
 
+
+
+  const winningCombo = [
+    [0, 1, 2],
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6],
+  ]
+
+
+
+function checkForWinner(player){
+  for (let combination of winningCombo){
+    let isWinningCombo = true;
+    for (let position of combination){
+      if (board[position] !== player){
+        isWinningCombo = false;
+        break;
+      }
+    }
+    if (isWinningCombo){
+      return true;
+    }
+
+  }
+  return false;
+}
+
+function updateBoard(tileIndex, currentPlayerSymbol){
+  board[tileIndex] = currentPlayerSymbol;
+}
+
+
+
   board.forEach((item, index) => {
       const tile = document.createElement('div');
       tile.className = 'tile';
@@ -63,16 +101,25 @@ const gameBoard = (() => {
 
   
   return {
-      board: board,
-      tiles: tiles
+      board,
+      updateBoard,
+      checkForWinner,
+      
   };
 })();
 
-console.log(gameBoard);
+
 
 
   ////////// DISPLAY \\\\\\\\\\
 
   const infoDisplay = document.getElementById('info');
   
-  
+
+console.log(gameBoard);
+gameBoard.updateBoard(0, "X");
+gameBoard.updateBoard(1, "X");
+gameBoard.updateBoard(2, "X");
+const playerXWins = gameBoard.checkForWinner('X');
+
+console.log(playerXWins);
